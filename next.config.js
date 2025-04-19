eab5a29ca8d2fb19a5d390ae18fdb9329f47b09d
Adil Parwez <dlprwz@gmail.com>
2025-04-19 18:53:50 +0530

    deployemnt fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
   // This forces Next.js to use Server Components for all routes
   // which makes handling server-only dependencies easier
   experimental: {
